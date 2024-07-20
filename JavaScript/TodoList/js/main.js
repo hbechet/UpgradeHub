93f5e9e965d3e3c7d173c6902dd0365c3223a9e0
Haritz Bechet <haritz.bechet@gmail.com>
2024-07-20 10:15:01 +0200

    JS base and classes created

---
+++ JavaScript/TodoList/js/main.js
@@ -0,0 +1,24 @@
+class Todo {
+
+    constructor(todo) {
+        this.definition = todo;
+    }
+}
+
+class List {
+
+    constructor() {
+        this.todoList = [];
+    }
+}
+
+let text = document.querySelector('input');
+
+text.addEventListener('change', event => event.target.value);
+
+
+let button = documen.querySelector('.btn-add');
+
+button.addEventListener('submit', event => {
+
+});
