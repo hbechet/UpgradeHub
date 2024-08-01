0fb4888aa90125c9c50e7d940e74aa5485fd2900
Haritz Bechet <haritz.bechet@gmail.com>
2024-08-01 20:19:02 +0200

    Installed Swal and form created

--- Angular_Blog/src/app/pages/formulario/formulario.component.ts
+++ Angular_Blog/src/app/pages/formulario/formulario.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PostService } from '../../services/post.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+
+  postService = inject(PostService);
+
+  newForm: FormGroup = new FormGroup({
+    titulo: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    texto: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(100)
+    ]),
+    autor: new FormControl(),
+    imagen: new FormControl(),
+    categoria: new FormControl(),
+  });
+
+  onSubmit() {
+    if (this.newForm.valid)
+      console.log(this.newForm.value);
+    // this.postService.create(this.newForm);
+  }
 
 }
